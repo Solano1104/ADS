0d0397edd59322792456e55b11bd5a2d91e3c7c3
Solano1104 <solanojoaopedro@gmail.com>
2026-08-11 20:55:12 -0400

    hhgfgfhgfhghg

---
+++ spring boot/src/main/java/br/com/solano/PO2/Po2Application.java
@@ -0,0 +1,13 @@
+package br.com.solano.PO2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Po2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Po2Application.class, args);
+	}
+
+}
---
+++ spring boot/src/test/java/br/com/solano/PO2/Po2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solano.PO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Po2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
